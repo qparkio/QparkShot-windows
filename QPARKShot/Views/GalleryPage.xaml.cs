68ecb32ce76599da01c7e0d24428fc7977d83695
QPARK <work@qpark.io>
2026-06-22 23:22:35 +0300

    GalleryPage: add missing 'using QPARKShot.Helpers;' for Logger

--- QPARKShot/Views/GalleryPage.xaml.cs
+++ QPARKShot/Views/GalleryPage.xaml.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using QPARKShot.Helpers;
 using QPARKShot.Services;
 
 namespace QPARKShot.Views;
